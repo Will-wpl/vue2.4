271dfeab66ba49fd614c99324c16714af1b415c6
Justin Liu <justin.f.liu@avanade.com>
2019-10-06 13:42:48 +0800

    import bootstrap

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import ElementUI from 'element-ui';
-import BootstrapVue from 'bootstrap-vue'
 import 'element-ui/lib/theme-chalk/index.css';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(ElementUI);
 Vue.use(BootstrapVue);
