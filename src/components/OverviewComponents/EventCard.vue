a51bbfc094efe6908bde185378ce60b399e70650
Justin Liu <justin.f.liu@avanade.com>
2019-10-08 14:06:29 +0800

    Added EventCard

---
+++ src/components/OverviewComponents/EventCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <router-link class="event-link" :to="{ name: 'Overview', params: { id: event.id } }">
+    <div class="event-card -shadow">
+        
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h4 class="title">{{ event.title }}</h4>
+      <!-- <BaseIcon name="users">{{ event.attendees.length }} attending</BaseIcon> -->
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    event: Object
+  }
+};
+</script>
+
+<style scoped>
+.event-card {
+  width: 394px;
+  padding: 20px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+  background-color: #0066cc;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+  margin: 0;
+}
+.event-link {
+  color: #ffffff;
+  text-decoration: none;
+  font-weight: 100;
+}
+</style>
--- src/views/OverviewPage/Overview.vue
+++ src/views/OverviewPage/Overview.vue
@@ -1,32 +1,101 @@
 <template>
-    <el-main>
-      <div>
-        <!-- <img src="@/assets/DataMgtBackgroupPic.png" /> -->
-        <p>Justin Test</p>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <p>Justin Test</p>
-      </div>
-    </el-main>
+  <el-main>
+    <div>
+      <h3>系统实时动态概览</h3>
+      <EventCard v-for="event in events" :key="event.id" :event="event" />
+      <!-- <img src="@/assets/DataMgtBackgroupPic.png" /> -->
+
+    </div>
+  </el-main>
 </template> 
 
 <script>
+import EventCard from "@/components/OverviewComponents/EventCard.vue";
+
 export default {
-  name: "AppealOverview"
+  name: "AppealOverview",
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 1,
+          title: "Beach Cleanup",
+          date: "Aug 28 2018",
+          time: "10:00",
+          location: "Daytona Beach",
+          description: "Let's clean up this beach.",
+          organizer: "Adam Jahr",
+          category: "sustainability",
+          attendees: [
+            {
+              id: "abc123",
+              name: "Adam Jahr"
+            },
+            {
+              id: "def456",
+              name: "Gregg Pollack"
+            },
+            {
+              id: "ghi789",
+              name: "Beth Swanson"
+            },
+            {
+              id: "jkl101",
+              name: "Mary Gordon"
+            }
+          ]
+        },
+        {
+          id: 2,
+          title: "Park Cleanup",
+          date: "Nov 12 2018",
+          time: "12:00",
+          location: "132 N Magnolia Street, Orlando, Florida",
+          description: "We're going to clean up this park.",
+          organizer: "Adam Jahr",
+          category: "nature",
+          attendees: [
+            {
+              id: "ghi789",
+              name: "Beth Swanson"
+            },
+            {
+              id: "jkl101",
+              name: "Mary Gordon"
+            }
+          ]
+        }
+        // ,
+        // {
+        //   id: 3,
+        //   title: "Pet Adoption Day",
+        //   date: "Dec 2 2018",
+        //   time: "12:00",
+        //   location: "132 N Magnolia Street, Orlando, Florida",
+        //   description: "Help animals find new homes.",
+        //   organizer: "Gregg Pollack",
+        //   category: "animal welfare",
+        //   attendees: [
+        //     {
+        //       id: "abc123",
+        //       name: "Adam Jahr"
+        //     },
+        //     {
+        //       id: "ghi789",
+        //       name: "Beth Swanson"
+        //     },
+        //     {
+        //       id: "jkl101",
+        //       name: "Mary Gordon"
+        //     }
+        //   ]
+        // }
+      ]
+    };
+  }
 };
 </script>
 
@@ -36,12 +105,17 @@ export default {
   background-color: #2ae22a; */
 }
 
+h3{
+  text-align: left;
+  color:#014282;
+}
+
 .el-main {
   background-image: url("~@/assets/DataMgtBackgroupPic.png");
   background-repeat: no-repeat;
   background-attachment: scroll;
   background-position: center;
   background-size: 100%;
+  margin-right: 1%;
 }
-
 </style>
