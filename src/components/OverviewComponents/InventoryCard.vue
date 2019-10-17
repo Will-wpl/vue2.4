55ef9e1d4098b4f257cb3c3fdf5868ffb81bd2fb
Wang <peilong.wang@accenture.com>
2019-10-17 11:07:18 +0800

    修改首页对应图标

--- src/components/OverviewComponents/InventoryCard.vue
+++ src/components/OverviewComponents/InventoryCard.vue
@@ -1,17 +1,17 @@
 <template>
   <router-link class="event-link" :to="{ name: 'Overview', params: { id: event.id } }">
     <div class="event-card -shadow" :style="{height:cardheight+'px'}" v-if="type=='PurchaseSale'">
-      <div class="row">
-        <div class="col-sm col-md col-lg-6 el-icon-tickets">{{ event.title }}</div>
+      <div class="row alignCenter">
+        <div :class="['col-sm','col-md','col-lg-6','event-card-title',event.icon]">{{ event.title }}</div>
         <div class="col-sm col-md col-lg textR fontBig">{{ event.cyVal }}</div>
       </div>
       <div class="positionBottom">
         <div class="row">
-          <div class="col-sm col-md col-lg textC line">
+          <div class="col-sm col-md col-lg textC line nopadding">
             已反馈
             <span>{{ event.feedback }}</span>
           </div>
-          <div class="col-sm col-md col-lg textC">
+          <div class="col-sm col-md col-lg textC nopadding">
             未反馈
             <span>{{ event.unfeedback }}</span>
           </div>
@@ -19,8 +19,8 @@
       </div>
     </div>
     <div class="event-card -shadow" :style="{height:cardheight+'px'}" v-if="type=='Overview'">
-      <div class="row">
-        <div class="col-sm col-md col-lg-6 el-icon-tickets">{{ event.title }}</div>
+      <div class="row alignCenter">
+        <div :class="['col-sm','col-md','col-lg-6','event-card-title',event.icon]">{{ event.title }}</div>
         <div class="col-sm col-md col-lg textR fontBig">{{ event.cyVal }}</div>
       </div>
     </div>
@@ -29,9 +29,9 @@
       :style="{height:cardheight+'px'}"
       v-if="type=='certificateFile'"
     >
-      <div class="row">
-        <div class="col-sm col-md col-lg-12 el-icon-tickets">{{ event.title }}</div>
-        <div class="col-sm col-md col-lg-12 textR fontBig positionBottom">{{ event.cyVal }}</div>
+      <div class="row alignCenter">
+        <div :class="['col-sm','col-md','col-lg','event-card-title','nopadding']">{{ event.title }}</div>
+        <div class="col-sm col-md col-lg positionBottom"><p class="textR fontBig">{{ event.cyVal }}</p></div>
       </div>
     </div>
     <div
@@ -39,9 +39,9 @@
       :style="{height:cardheight+'px'}"
       v-if="type=='toBeconfirmed'"
     >
-      <div class="row">
-        <div class="col-sm col-md col-lg-12 el-icon-tickets">{{ event.title }}</div>
-        <div class="col-sm col-md col-lg-12 textR fontBig positionBottom">{{ event.cyVal }}</div>
+      <div class="row alignCenter">
+        <div :class="['col-sm','col-md','col-lg','event-card-title','nopadding']">{{ event.title }}</div>
+        <div class="col-sm col-md col-lg-12 positionBottom"><p class="textR fontBig">{{ event.cyVal }}</p></div>
       </div>
     </div>
   </router-link>
@@ -58,11 +58,38 @@ export default {
 </script>
 
 <style scoped>
+.event-card-title {
+  font-size: 20px;
+  padding-left: 30px;
+}
+.nopadding{
+  padding: 0;
+}
+.alignCenter {
+  align-items: center;
+  margin: 0;
+}
+.iconycy {
+  background: url("~@/assets/iconycy.png") left no-repeat;
+}
+.iconwcy{
+  background: url("~@/assets/iconwcy.png") left no-repeat;
+}
+.iconsync1 {
+  background: url("~@/assets/iconsync1.png") left no-repeat;
+}
+.iconsync2 {
+  background: url("~@/assets/iconsync2.png") left no-repeat;
+}
+.iconsync3 {
+  background: url("~@/assets/iconsync3.png") left no-repeat;
+}
 .el-icon-tickets:before {
   margin-right: 8px;
 }
 .textR {
   text-align: right;
+  padding: 0;
 }
 .textC {
   text-align: center;
@@ -71,7 +98,7 @@ export default {
   border-right: 1px solid #ddd;
 }
 .fontBig {
-  font-size: 30px;
+  font-size: 32px;
 }
 .el-icon-tickets {
   line-height: 44px;
@@ -106,10 +133,12 @@ export default {
   position: absolute;
   bottom: 20px;
   left: 0px;
+  padding: 0px 20px;
 }
 .positionBottom span {
   font-size: 25px;
 }
+.positionBottom p {margin: 0;}
 .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
