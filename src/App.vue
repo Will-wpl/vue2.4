ce47c74ee6af711a29dd5028098cce0600deac6d
Justin Liu <justin.f.liu@avanade.com>
2019-10-20 13:24:10 +0800

    change to login status for debugging before formal backend service deployment

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div v-if="loggedIn" class="wholeH">
+    <div v-if="loginStatus" class="wholeH">
       <!-- <el-container id="header"> -->
       <el-row>
         <el-col :span="10">
@@ -33,7 +33,7 @@
       </el-container>
     </div>
     <!-- <Layout /> -->
-    <Login v-if="!loggedIn" @go="showMainPage" />
+    <Login v-if="!loginStatus" @go="showMainPage" />
   </div>
 </template>
 
