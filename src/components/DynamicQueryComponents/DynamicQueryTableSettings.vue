845faa2db4be18f52c7df2616280369406171d63
Justin Liu <justin.f.liu@avanade.com>
2019-10-22 10:52:39 +0800

    Modified alignment for DynamicQueryTbl Drawer

--- src/components/DynamicQueryComponents/DynamicQueryTableSettings.vue
+++ src/components/DynamicQueryComponents/DynamicQueryTableSettings.vue
@@ -4,103 +4,103 @@
     <div class="container">
       <span>商业信息</span>
       <div class="row">
-        <div class="col-4 col-md-6 col-sm-6">
+        <div class="col-lg-4 col-md-6 col-sm-6">
           <el-checkbox v-model="checked">商业代码</el-checkbox>
         </div>
-        <div class="col-4 col-md-6 col-sm-6">
+        <div class="col-lg-4 col-md-6 col-sm-6">
           <el-checkbox v-model="checked">经销商名称</el-checkbox>
         </div>
-        <div class="col-4">
+        <div class="col-lg-4">
           <el-checkbox v-model="checked">大区</el-checkbox>
         </div>
       </div>
       <div class="row">
-        <div class="col-4 col-md-6 col-sm-6">
+        <div class="col-lg-4 col-md-6 col-sm-6">
           <el-checkbox v-model="checked">区域</el-checkbox>
         </div>
-        <div class="col-4 col-md-6 col-sm-6">
+        <div class="col-lg-4 col-md-6 col-sm-6">
           <el-checkbox v-model="checked">省份</el-checkbox>
         </div>
-        <div class="col-4">
+        <div class="col-lg-4">
           <el-checkbox v-model="checked">城市</el-checkbox>
         </div>
       </div>
       <div class="row">
-        <div class="col-4col-md-6 col-sm-6">
+        <div class="col-lg-4 col-md-6 col-sm-6">
           <el-checkbox v-model="checked">多渠道准入代表</el-checkbox>
         </div>
-        <div class="col-4col-md-6 col-sm-6">
+        <div class="col-lg-4 col-md-6 col-sm-6">
           <el-checkbox v-model="checked">数据时间</el-checkbox>
         </div>
-        <div class="col-4">
+        <div class="col-lg-4">
           <el-checkbox v-model="checked">等级</el-checkbox>
         </div>
       </div>
       <hr />
       <span>产品信息</span>
       <div class="row">
-        <div class="col-4 col-md-6 col-sm-6">
+        <div class="col-lg-4 col-md-6 col-sm-6">
           <el-checkbox v-model="checked">产品代码</el-checkbox>
         </div>
-        <div class="col-4 col-md-6 col-sm-6">
+        <div class="col-lg-4 col-md-6 col-sm-6">
           <el-checkbox v-model="checked">产品</el-checkbox>
         </div>
-        <div class="col-4">
+        <div class="col-lg-4">
           <el-checkbox v-model="checked">品牌</el-checkbox>
         </div>
       </div>
       <hr />
       <span>数据信息</span>
       <div class="row">
-        <div class="col-4 col-md-6 col-sm-6">
+        <div class="col-lg-4 col-md-6 col-sm-6">
           <el-checkbox v-model="checked">期初逻辑库存</el-checkbox>
         </div>
-        <div class="col-4 col-md-6 col-sm-6">
+        <div class="col-lg-4 col-md-6 col-sm-6">
           <el-checkbox v-model="checked">逻辑采购</el-checkbox>
         </div>
-        <div class="col-4">
+        <div class="col-lg-4">
           <el-checkbox v-model="checked">销售数据</el-checkbox>
         </div>
       </div>
       <div class="row">
-        <div class="col-4 col-md-6 col-sm-6">
+        <div class="col-lg-4 col-md-6 col-sm-6">
           <el-checkbox v-model="checked">调整库存</el-checkbox>
         </div>
-        <div class="col-4 col-md-6 col-sm-6">
+        <div class="col-lg-4 col-md-6 col-sm-6">
           <el-checkbox v-model="checked">逻辑库存</el-checkbox>
         </div>
-        <div class="col-4">
+        <div class="col-lg-4">
           <el-checkbox v-model="checked">实际库存</el-checkbox>
         </div>
       </div>
       <div class="row">
-        <div class="col-6 col-md-12 col-sm-12">
+        <div class="col-lg-6 col-md-12 col-sm-12">
           <el-checkbox v-model="checked">库存差异逻辑VS实际</el-checkbox>
         </div>
-        <div class="col-3"></div>
-        <div class="col-3"></div>
+        <div class="col-lg-3"></div>
+        <div class="col-lg-3"></div>
       </div>
       <hr />
       <span>历史数据</span>
       <div class="row">
-        <div class="col-4 col-md-6 col-sm-6">
+        <div class="col-lg-4 col-md-6 col-sm-6">
           <el-checkbox v-model="checked">M-3库存调整量</el-checkbox>
         </div>
-        <div class="col-4 col-md-6 col-sm-6">
+        <div class="col-lg-4 col-md-6 col-sm-6">
           <el-checkbox v-model="checked">M-2库存调整量</el-checkbox>
         </div>
-        <div class="col-4">
+        <div class="col-lg-4">
           <el-checkbox v-model="checked">M-1库存调整量</el-checkbox>
         </div>
       </div>
       <hr />
       <span>申请状态</span>
       <div class="row">
-        <div class="col-6 col-md-12 col-sm-12">
+        <div class="col-lg-6 col-md-12 col-sm-12">
           <el-checkbox v-model="checked">是否已申请系统数据调整更新</el-checkbox>
         </div>
-        <div class="col-3"></div>
-        <div class="col-3"></div>
+        <div class="col-lg-3"></div>
+        <div class="col-lg-3"></div>
       </div>
     </div>
 
