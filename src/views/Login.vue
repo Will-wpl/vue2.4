74f97ece2210c274bc4616ae67fd4cdb89e5bde6
Justin Liu <justin.f.liu@avanade.com>
2019-10-22 16:18:27 +0800

    Reorganised the Login Page to fill in the whole bkg picture

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,65 +1,97 @@
 <template>
-  <el-main>
-    <el-col class="loginWhole">
-      <h4 class="login-top">
-        <img src="@/assets/RoucheBrandMark.png" />渠道数据管理平台
-      </h4>
-      <el-col class="login-title">
-        渠道数据管理平台
-        <span>Channel Data Management Platform</span>
-      </el-col>
-      <form @submit.prevent="login">
-        <el-col class="login-info">
-          <el-errorMsg
-            :error="$v.userName.$error"
-            type
-            :msgList="[
-          {
-            show:!$v.userName.required,
-            text:'用户名不能为空'}
-          ]"
-          >
-            <el-input
-              placeholder="用户名"
-              class="input-inner"
-              prefix-icon="el-icon-s-custom"
-              v-model="userName"
-            ></el-input>
-          </el-errorMsg>
-          <el-errorMsg
-            :error="$v.password.$error"
-            type
-            :msgList="[
-          {
-            show:!$v.password.required,
-            text:'密码不能为空'}
-          ]"
-          >
-            <el-input
-              type="password"
-              class="input-inner"
-              placeholder="密码"
-              prefix-icon="el-icon-key"
-              v-model="password"
-            ></el-input>
-          </el-errorMsg>
-          <!-- <el-button type="primary" round @click="goPage">登 录</el-button> -->
-          <el-button type="primary" round @click="login">登 录</el-button>
-          <!-- <button type="submit" name="button">登 录</button> -->
-        </el-col>
-      </form>
-    </el-col>
-  </el-main>
-</template> 
+  <div id="wrapper" class="container-fluid">
+    <div class="row">
+      <div class="col-lg-4 col-sm-12">
+        <div class="container-fluid">
+          <div class="row">
+            <div style class="col-lg-4 col-sm-12 headerIcon">
+              <img src="../assets/icon_logo_b.png" />
+              <span class="dms-login-logo-tt">CDMS</span>
+            </div>
+          </div>
+
+          <div class="row mt">
+            <div class="dms-login-content-tt">
+              <span class="dms-login-content-largett">商业管理系统</span>
+              <p>China Dealer Management System</p>
+            </div>
+          </div>
+          <div class="row marginTop">
+            <div class="container">
+              <div class="row">
+                <div class="col-2"></div>
+                <div class="col-8">
+                  <el-errorMsg
+                    :error="$v.userName.$error"
+                    :msgList="[
+                    {
+                      show:!$v.userName.required,
+                      text:'用户名不能为空'}
+                    ]"
+                  />
+                    <el-input
+                      placeholder="用户名"
+                      class="formInput"
+                      prefix-icon="el-icon-s-custom"
+                      v-model="userName"
+                    ></el-input>
+                  
+                </div>
+                <div class="col-2"></div>
+              </div>
+              <div class="row marginTop">
+                <div class="col-2"></div>
+                <div class="col-8">
+                  <el-errorMsg
+                    :error="$v.password.$error"
+                    :msgList="[
+                      {
+                        show:!$v.password.required,
+                        text:'密码不能为空'}
+                      ]"
+                  />
+                    <el-input
+                      type="password"
+                      class="formInput"
+                      placeholder="密码"
+                      prefix-icon="el-icon-key"
+                      v-model="password"
+                    ></el-input>
+                  
+                </div>
+                <div class="col-2"></div>
+              </div>
+
+              <div class="row marginTop">
+                <div class="col-2"></div>
+                <div class="col-8">
+                  <el-button class="login-btn" type="primary" round @click="login">登 录</el-button>
+                </div>
+                <div class="col-2"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="leftBg" class="col-lg-8"></div>
+    </div>
+  </div>
+</template>
 
 <script>
 import { required } from "vuelidate/lib/validators";
 import store from "@/store/store";
 export default {
   name: "Login",
-  components: {},
+  data() {
+    return {
+      userName: "",
+      password: "",
+      error: null
+    };
+  },
   methods: {
-    goPage() {
+    login() {
       this.$v.userName.$touch();
       if (!this.$v.userName.required) {
         return;
@@ -68,10 +100,6 @@ export default {
       if (!this.$v.password.required) {
         return;
       }
-      sessionStorage.loginToken = this.userName;
-      this.$emit("go");
-    },
-    login() {
       store
         .dispatch("login/login", {
           userName: this.userName,
@@ -88,11 +116,6 @@ export default {
         });
     }
   },
-  mounted() {
-    if (sessionStorage.loginToken) {
-      this.$emit("go");
-    }
-  },
   validations: {
     userName: {
       required
@@ -100,64 +123,119 @@ export default {
     password: {
       required
     }
-  },
-  data() {
-    return {
-      userName: "",
-      password: "",
-      error: null
-    };
   }
 };
 </script>
-<style>
-html,
-body,
-#app {
-  height: 100%;
-  overflow: hidden;
+
+<style >
+#leftBg {
+  background-image: url("~@/assets/background_login.png");
+  background-repeat: no-repeat;
+  background-attachment: scroll;
+  background-position: center;
+  background-size: cover;
 }
-.input-inner input {
+
+.marginTop {
+  margin-top: 10px;
+}
+
+.formInput input {
   border-radius: 100px !important;
   border-color: #bcdbf6 !important;
 }
-.input-inner i {
+.formInput i {
   color: #5888be !important;
 }
-</style>
-<style scoped>
-.loginWhole {
+
+.mt {
+  margin-top: 50px;
+}
+
+.dms-login-content {
+  position: relative;
+  width: 38%;
   height: 100%;
   background: #fff;
-  width: 35%;
+  padding: 20px 50px 100px 20px;
 }
-.login-top {
-  margin: 30px 20px;
+
+.dms-login-logo {
+  margin-bottom: 60px;
 }
-.login-top img {
-  width: 75px;
-  margin-right: 10px;
-}
-.login-title {
-  text-align: center;
-  font-size: 25px;
+
+.dms-login-content-tt {
+  margin-left: 90px;
+  color: #007ac3;
   font-weight: bold;
-  margin: 60px auto;
-  color: #1c5ca3;
+  font-size: 24px;
+  margin-bottom: 40px;
 }
-.login-title span {
+
+.dms-login-content-tt p {
+  font-size: 14px;
+  margin-top: 8px;
+}
+.dms-login-content-largett {
+  font-size: 20px;
+  letter-spacing: 12px;
+  margin-left: 8%;
+}
+
+.dms-login-content .dms-icon-user,
+.dms-login-content .dms-icon-pass {
+  margin-left: 100px;
+  position: relative;
+  top: 33px;
+}
+
+.dms-login-input {
   display: block;
-  font-size: 15px;
-  margin-top: 10px;
+  width: 250px;
+  height: 38px;
+  line-height: 38px;
+  border: 0;
+  border: 1px solid #9ac8e7;
+  border-radius: 30px;
+  background: none;
+  outline: none;
+  margin-bottom: 10px;
+  padding-left: 40px;
+  margin-left: 90px;
+  font-size: 14px;
+  line-height: 34px;
 }
-.login-info {
-  width: 46%;
-  margin: 70px auto;
+
+html,
+body {
+  height: 100%;
+  margin: 0;
+}
+
+#app {
+  height: 100%;
+}
+
+#wrapper {
+  height: 100%;
+}
+
+#wrapper > div {
+  height: 100%;
+}
+
+.headerIcon {
+  margin-top: 3%;
+}
+
+.login-btn {
   display: block;
-  float: none;
-}
-.login-info button {
+  color: #fff !important;
   width: 100%;
+  border-radius: 20px;
+  line-height: 36px;
+  text-align: center;
+  cursor: pointer;
   background: -webkit-linear-gradient(
     left,
     rgba(42, 143, 226),
@@ -174,19 +252,16 @@ body,
     rgba(27, 62, 177)
   ); /* Firefox 3.6 - 15 */
   background: linear-gradient(to right, #2a8fe2, #1b3eb1);
-  outline: none;
+  margin-left: 90px;
+  margin-top: 30px;
 }
-h4 {
-  text-align: left;
-  color: #014282;
-}
-.el-main {
-  background-image: url("~@/assets/background_login.png");
-  background-repeat: no-repeat;
-  background-attachment: scroll;
-  background-position: center;
-  background-size: cover;
-  height: 100%;
-  padding: 0;
+
+.dms-login-logo-tt {
+  color: #007ac3;
+  font-weight: bold;
+  font-size: 20px;
+  margin-left: 10px;
+  margin-top: 5px;
+  position: absolute;
 }
 </style>
